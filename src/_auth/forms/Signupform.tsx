17373be4ca826a144b41efedb0486b3c3ed54c97
Himanish15 <amiteshloll@gmail.com>
2023-12-18 14:03:52 +0400

    Basic setup

---
+++ src/_auth/forms/Signupform.tsx
@@ -0,0 +1,11 @@
+
+import { Button } from "@/components/ui/button"
+const signupform = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default signupform
---
+++ src/_root/Rootlayout.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Rootlayout = () => {
+  return (
+    <div>Rootlayout</div>
+  )
+}
