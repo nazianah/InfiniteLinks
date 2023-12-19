5bb943b95d06bdc7ebab28e36490ce42e032659c
Yu <awushu6@gmail.com>
2023-12-19 15:06:37 +0400

    Update configuration files and fix styling issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
 import { Routes, Route } from "react-router-dom";
 
+import {
+  Home,
+  Explore,
+  Saved,
+  CreatePost,
+  Profile,
+  EditPost,
+  PostDetails,
+  UpdateProfile,
+  AllUsers,
+} from "@/_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignupForm from "@/_auth/forms/SignupForm";
+import SigninForm from "@/_auth/forms/SigninForm";
+import { Toaster } from "@/components/ui/toaster";
+
 import "./globals.css";
-import SigninForm from "./_auth/forms/Signinform";
-import SignupForm from "./_auth/forms/Signupform";
-import { Home } from "./_root/pages/";
-import "./globals.css";
-import { Authlayout } from "./_auth/Authlayout";
-import { Rootlayout } from "./_root/Rootlayout";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
-        {/* pubilc routes */}
-        <Route element={<Authlayout />}>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SigninForm />} />
-          <Route path="/sign-in" element={<SignupForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
         </Route>
 
         {/* private routes */}
-        <Route element={<Rootlayout />}>
+        <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
+
+      <Toaster />
     </main>
   );
 };
